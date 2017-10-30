3dfd6da1c3bb98427ef74913ebe0e9583c14eb2e
Sijing Huang <sijing.huang@epfl.ch>
2017-10-30 09:24:06 +0100

    Fichiers primitifs pour installer Grafana et InfluxDB

---
+++ install_influxDB.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Ajouter la repo
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+#installer influxdb
+sudo apt-get update && sudo apt-get install influxdb
+
